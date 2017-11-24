c0960ea288ed18b8a7ff18904a92087061da7979
Carlos Vega <clmvega@gmail.com>
2017-11-23 19:12:08 -0600

    first commit

---
+++ src/android/EventReceiver.java
@@ -0,0 +1,24 @@
+package com.alterx.cordova.geofence.receiver;
+
+import android.util.Log;
+import com.cowbell.cordova.geofence.Gson;
+import com.cowbell.cordova.geofence.GeoNotification;
+
+public class TransitionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String error = intent.getStringExtra("error");
+
+        if (error != null) {
+            //handle error
+            Log.println(Log.ERROR, "YourAppTAG", error);
+        } else {
+            String geofencesJson = intent.getStringExtra("transitionData");
+            GeoNotification[] geoNotifications = Gson.get().fromJson(geofencesJson, GeoNotification[].class);
+            //handle geoNotifications objects
+
+            Log.println(Log.INFO, "YourAppTAG", "hello");
+        }
+    }
+}
